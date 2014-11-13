b54ae4b4bec151fd167a854a60c9c5decc961cc7
Jochen Deferme <jochen.deferme@gmail.com>
2014-11-13 23:02:23 +0100

    method length and ancestor are working de iterable version still need to be done

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -7,23 +7,31 @@ public class SAP {
    public SAP(Digraph G) {
 	   DG = new Digraph(G);
 	   
-	   Map<Integer, int[]> map = BFS(9);
-//	   for( int[] vc : map.values()) {
-//		   System.out.println("");
+	   //test BFS
+//	   Map<Integer, Integer> map = BFS(5);
+//	   for( int i=0; i< DG.V();i++) {
+//		   System.out.println("key: " + i + "\tvalue: " + map.get(i));
 //	   }
-	   System.out.println(map.values());
+//	   System.out.println(map.values());
+
    }
 
    // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-	   
-	   return -1;
+   public int length(int v, int w) { //these methods don't work if it is not a DAG
+	   Map<Integer, Integer> mapv = BFS(v);
+	   int[] sol = BFS2(w, mapv);
+	   if (sol[0] == -1)
+		   return -1;
+	   return sol[1];
    }
 
    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
    public int ancestor(int v, int w) {
-	   
-	   return -1;
+	   Map<Integer, Integer> mapv = BFS(v);
+	   int[] sol = BFS2(w, mapv);
+	   if (sol[0] == -1)
+		   return -1;
+	   return sol[0];
    }
 
    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -38,62 +46,72 @@ public class SAP {
 	   return -1;
    }
    
-   private Map<Integer, int[]> BFS(int v) { //visited and count is in the array
-//	   List<Integer> list = new ArrayList<Integer>();
-	   Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+   private Map<Integer, Integer> BFS(int v) { 
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	   java.util.Queue<Integer> q = new LinkedList<Integer>();
 	   int count = 0;
 	   
-	   q.add(v); //the first element is not in the map
+	   q.add(v);
+	   map.put(v, count);
+	   
 	   while(!q.isEmpty()) {
 		   int current = q.remove();
 		   
-		   if (!map.containsKey(current)) {
-			   int[] vc = new int[2];
-			   vc[0] = 1; // mark as visited
-			   vc[1] = count;
-			   map.put(current, vc);
-		   }
-		   
-		   count++; // when exactly to increment? 
+		   count = map.get(current) + 1; 
 		   
 		   for(int adj : DG.adj(current)) {
-			   if (map.containsKey(adj)) { //this might replace the vc[0]?
-//				   int[] vc = map.get(adj);
-//				   if (vc[0] == 0)  {//not yet visited
-//					   q.add(adj);
-//					   vc[0] = 1; // mark as visited
-//					   vc[1] = count;
-//				   }  
-			   }
-			   else {
-//				   int[] vc = new int[2];
+			   if (!map.containsKey(adj)) { 
+				   map.put(adj, count);
 				   q.add(adj);
-//				   vc[0] = 1; // mark as visited
-//				   vc[1] = count;
-//				   map.put(adj, vc);
 			   }
 		   }
-		   
 	   }
-	  
-	   
 	   return map;
+   }
+   
+   private int[] BFS2(int v, Map<Integer, Integer> mapv) { //BFS until reaching path of v
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	   java.util.Queue<Integer> q = new LinkedList<Integer>();
+	   int count = 0;
+	   int[] sol = {-1, Integer.MAX_VALUE};
+	   
+	   q.add(v);
+	   map.put(v, count);
+	   
+	   while(!q.isEmpty()) {
+		   int current = q.remove();
+		   
+		   if (mapv.containsKey(current)) { 
+			   if ((count + mapv.get(current)) < sol[1]) {
+				   sol[0] = current;
+				   sol[1] = map.get(current) + mapv.get(current);
+			   }
+		   }
+			   
+		   count = map.get(current) + 1; 
+		   
+		   for(int adj : DG.adj(current)) {
+			   if (!map.containsKey(adj)) { 
+				   map.put(adj, count);
+				   q.add(adj);
+			   }
+		   }
+	   }
+	   return sol;
    }
 
    // do unit testing of this class
    public static void main(String[] args) {
-	    In in = new In("testing/digraph1.txt"); //args[0]);
+	    In in = new In("testing/digraph2.txt"); //args[0]);
 	    Digraph G = new Digraph(in);
 	    SAP sap = new SAP(G);
-
 	    
-//	    while (!StdIn.isEmpty()) {
-//	        int v = StdIn.readInt();
-//	        int w = StdIn.readInt();
-//	        int length   = sap.length(v, w);
-//	        int ancestor = sap.ancestor(v, w);
-//	        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-//	    }
+	    while (!StdIn.isEmpty()) {
+	        int v = StdIn.readInt();
+	        int w = StdIn.readInt();
+	        int length   = sap.length(v, w);
+	        int ancestor = sap.ancestor(v, w);
+	        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+	    }
    }
 }
