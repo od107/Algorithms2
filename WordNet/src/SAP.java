f8f9dd2ade6465b415ca6b6d85fab0f9724df980
Jochen Deferme <jochen.deferme@gmail.com>
2014-11-14 19:44:13 +0100

    SAP seems to work completely, private methods can still be reworked to be more generic

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -7,8 +7,12 @@ public class SAP {
    public SAP(Digraph G) {
 	   DG = new Digraph(G);
 	   
-	   //test BFS
-//	   Map<Integer, Integer> map = BFS(5);
+//	   test BFS
+//	   List<Integer> v = new ArrayList<Integer>();
+//	   v.add(12);
+//	   v.add(2);
+//	   v.add(3);
+//	   Map<Integer, Integer> map = BFS(v);
 //	   for( int i=0; i< DG.V();i++) {
 //		   System.out.println("key: " + i + "\tvalue: " + map.get(i));
 //	   }
@@ -36,32 +40,35 @@ public class SAP {
 
    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-	   
-	   return -1;
+	   Map<Integer, Integer> mapv = BFS(v);
+	   int[] sol = BFS2(w, mapv);
+	   if (sol[0] == -1)
+		   return -1;
+	   return sol[1];
    }
 
    // a common ancestor that participates in shortest ancestral path; -1 if no such path
    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-	   
-	   return -1;
+	   Map<Integer, Integer> mapv = BFS(v);
+	   int[] sol = BFS2(w, mapv);
+	   if (sol[0] == -1)
+		   return -1;
+	   return sol[0];
    }
    
    private Map<Integer, Integer> BFS(int v) { 
 	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	   java.util.Queue<Integer> q = new LinkedList<Integer>();
-	   int count = 0;
 	   
 	   q.add(v);
-	   map.put(v, count);
+	   map.put(v, 0);
 	   
 	   while(!q.isEmpty()) {
 		   int current = q.remove();
 		   
-		   count = map.get(current) + 1; 
-		   
 		   for(int adj : DG.adj(current)) {
 			   if (!map.containsKey(adj)) { 
-				   map.put(adj, count);
+				   map.put(adj, map.get(current) + 1);
 				   q.add(adj);
 			   }
 		   }
@@ -72,27 +79,26 @@ public class SAP {
    private int[] BFS2(int v, Map<Integer, Integer> mapv) { //BFS until reaching path of v
 	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	   java.util.Queue<Integer> q = new LinkedList<Integer>();
-	   int count = 0;
 	   int[] sol = {-1, Integer.MAX_VALUE};
 	   
 	   q.add(v);
-	   map.put(v, count);
+	   map.put(v, 0);
 	   
 	   while(!q.isEmpty()) {
 		   int current = q.remove();
 		   
-		   if (mapv.containsKey(current)) { 
-			   if ((count + mapv.get(current)) < sol[1]) {
+		   if (mapv.containsKey(current)) { //vertex already visited
+			   if ((map.get(current) + mapv.get(current)) < sol[1]) {
 				   sol[0] = current;
 				   sol[1] = map.get(current) + mapv.get(current);
 			   }
+			   else //if vertex is already visited with a better value do not add neighbours
+				   continue; // early stopping
 		   }
 			   
-		   count = map.get(current) + 1; 
-		   
 		   for(int adj : DG.adj(current)) {
 			   if (!map.containsKey(adj)) { 
-				   map.put(adj, count);
+				   map.put(adj, map.get(current) + 1);
 				   q.add(adj);
 			   }
 		   }
@@ -100,18 +106,74 @@ public class SAP {
 	   return sol;
    }
 
+   private Map<Integer, Integer> BFS(Iterable<Integer> list) { 
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	   java.util.Queue<Integer> q = new LinkedList<Integer>();
+
+	   for(int v : list) {
+		   q.add(v);
+		   map.put(v, 0);
+	   }
+
+	   while(!q.isEmpty()) {
+		   int current = q.remove();
+
+		   for(int adj : DG.adj(current)) {
+			   if (!map.containsKey(adj)) { // || map.get(adj) > map.get(current)) { 
+				   map.put(adj, map.get(current) + 1);
+				   q.add(adj);
+			   }
+		   }
+	   }
+	   return map;   
+   }
+
+   private int[] BFS2(Iterable<Integer> list, Map<Integer, Integer> mapv) { //BFS until reaching path of v
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	   java.util.Queue<Integer> q = new LinkedList<Integer>();
+	   int[] sol = {-1, Integer.MAX_VALUE};
+	   
+	   for (int v : list) {
+		   q.add(v);
+		   map.put(v, 0);
+	   }
+	   
+	   while(!q.isEmpty()) {
+		   int current = q.remove();
+		   
+		   if (mapv.containsKey(current)) { //vertex already visited
+			   if ((map.get(current) + mapv.get(current)) < sol[1]) {
+				   sol[0] = current;
+				   sol[1] = map.get(current) + mapv.get(current);
+			   }
+			   else //if vertex is already visited with a better value do not add neighbours
+				   continue; // early stopping
+		   }
+			   
+		   for(int adj : DG.adj(current)) {
+			   if (!map.containsKey(adj)) { 
+				   map.put(adj, map.get(current) + 1);
+				   q.add(adj);
+			   }
+		   }
+	   }
+	   return sol;
+   }
+   
+   
    // do unit testing of this class
    public static void main(String[] args) {
-	    In in = new In("testing/digraph2.txt"); //args[0]);
+	    In in = new In("testing/digraph1.txt"); //args[0]);
 	    Digraph G = new Digraph(in);
 	    SAP sap = new SAP(G);
 	    
-	    while (!StdIn.isEmpty()) {
-	        int v = StdIn.readInt();
-	        int w = StdIn.readInt();
-	        int length   = sap.length(v, w);
-	        int ancestor = sap.ancestor(v, w);
-	        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-	    }
+//	    while (!StdIn.isEmpty()) {
+//	        int v = StdIn.readInt();
+//	        int w = StdIn.readInt();
+//	        int length   = sap.length(v, w);
+//	        int ancestor = sap.ancestor(v, w);
+//	        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+//	    }
+	    
    }
 }
