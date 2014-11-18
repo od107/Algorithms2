e8fd73e0755170c823076587370903f3743ad9fb
Jochen Deferme <jochen.deferme@gmail.com>
2014-11-18 22:42:29 +0100

    Cleaned up SAP: now only 1 BFS method left but ugly cast introduced because of assignment constraints

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -1,7 +1,6 @@
 import java.util.*;
-// TODO
-// the private methods can be reworked into 2 and probably 1 method
-// check efficiency first
+// ugly cast in length and ancestor is needed because the assignment
+// prohibits deviating from the API which includes external classes
 
 public class SAP {
 	private Digraph DG;
@@ -9,42 +8,33 @@ public class SAP {
    // constructor takes a digraph (not necessarily a DAG)
    public SAP(Digraph G) {
 	   DG = new Digraph(G);
-	   
-//	   test BFS
-//	   List<Integer> v = new ArrayList<Integer>();
-//	   v.add(12);
-//	   v.add(2);
-//	   v.add(3);
-//	   Map<Integer, Integer> map = BFS(v);
-//	   for( int i=0; i< DG.V();i++) {
-//		   System.out.println("key: " + i + "\tvalue: " + map.get(i));
-//	   }
-//	   System.out.println(map.values());
-
    }
 
    // length of shortest ancestral path between v and w; -1 if no such path
    public int length(int v, int w) { //these methods don't work if it is not a DAG
-	   Map<Integer, Integer> mapv = BFS(v);
-	   int[] sol = BFS2(w, mapv);
-	   if (sol[0] == -1)
-		   return -1;
-	   return sol[1];
+	   List<Integer> vlist = new ArrayList<Integer>();
+	   vlist.add(v);
+	   List<Integer> wlist = new ArrayList<Integer>();
+	   wlist.add(w);
+	   return length(vlist,wlist);
    }
 
    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
    public int ancestor(int v, int w) {
-	   Map<Integer, Integer> mapv = BFS(v);
-	   int[] sol = BFS2(w, mapv);
-	   if (sol[0] == -1)
-		   return -1;
-	   return sol[0];
+	   List<Integer> vlist = new ArrayList<Integer>();
+	   vlist.add(v);
+	   List<Integer> wlist = new ArrayList<Integer>();
+	   wlist.add(w);
+	   return ancestor(vlist,wlist);
    }
 
    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-	   Map<Integer, Integer> mapv = BFS(v);
-	   int[] sol = BFS2(w, mapv);
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	   ArrayList<Object> result = BFS(v, map);
+	   Map<Integer, Integer> mapv = (Map<Integer, Integer>) result.get(0);
+	   result = BFS(w, mapv);
+	   int[] sol = (int[]) result.get(1);
 	   if (sol[0] == -1)
 		   return -1;
 	   return sol[1];
@@ -52,88 +42,20 @@ public class SAP {
 
    // a common ancestor that participates in shortest ancestral path; -1 if no such path
    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-	   Map<Integer, Integer> mapv = BFS(v);
-	   int[] sol = BFS2(w, mapv);
+	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	   ArrayList<Object> result = BFS(v, map);
+	   Map<Integer, Integer> mapv = (Map<Integer, Integer>) result.get(0);
+	   result = BFS(w, mapv);
+	   int[] sol = (int[]) result.get(1);
 	   if (sol[0] == -1)
 		   return -1;
 	   return sol[0];
    }
    
-   private Map<Integer, Integer> BFS(int v) { 
+   private ArrayList<Object> BFS(Iterable<Integer> list, Map<Integer, Integer> mapv) { //BFS until reaching path of v
 	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	   java.util.Queue<Integer> q = new LinkedList<Integer>();
-	   
-	   q.add(v);
-	   map.put(v, 0);
-	   
-	   while(!q.isEmpty()) {
-		   int current = q.remove();
-		   
-		   for(int adj : DG.adj(current)) {
-			   if (!map.containsKey(adj)) { 
-				   map.put(adj, map.get(current) + 1);
-				   q.add(adj);
-			   }
-		   }
-	   }
-	   return map;
-   }
-   
-   private int[] BFS2(int v, Map<Integer, Integer> mapv) { //BFS until reaching path of v
-	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-	   java.util.Queue<Integer> q = new LinkedList<Integer>();
-	   int[] sol = {-1, Integer.MAX_VALUE};
-	   
-	   q.add(v);
-	   map.put(v, 0);
-	   
-	   while(!q.isEmpty()) {
-		   int current = q.remove();
-		   
-		   if (mapv.containsKey(current)) { //vertex already visited
-			   if ((map.get(current) + mapv.get(current)) < sol[1]) {
-				   sol[0] = current;
-				   sol[1] = map.get(current) + mapv.get(current);
-			   }
-//			   else //if vertex is already visited with a better value do not add neighbours
-//				   continue; // early stopping
-		   }
-			   
-		   for(int adj : DG.adj(current)) {
-			   if (!map.containsKey(adj)) { 
-				   map.put(adj, map.get(current) + 1);
-				   q.add(adj);
-			   }
-		   }
-	   }
-	   return sol;
-   }
-
-   private Map<Integer, Integer> BFS(Iterable<Integer> list) { 
-	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-	   java.util.Queue<Integer> q = new LinkedList<Integer>();
-
-	   for(int v : list) {
-		   q.add(v);
-		   map.put(v, 0);
-	   }
-
-	   while(!q.isEmpty()) {
-		   int current = q.remove();
-
-		   for(int adj : DG.adj(current)) {
-			   if (!map.containsKey(adj)) { // || map.get(adj) > map.get(current)) { 
-				   map.put(adj, map.get(current) + 1);
-				   q.add(adj);
-			   }
-		   }
-	   }
-	   return map;   
-   }
-
-   private int[] BFS2(Iterable<Integer> list, Map<Integer, Integer> mapv) { //BFS until reaching path of v
-	   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-	   java.util.Queue<Integer> q = new LinkedList<Integer>();
+	   ArrayList<Object> result = new ArrayList<Object>();
 	   int[] sol = {-1, Integer.MAX_VALUE};
 	   
 	   for (int v : list) {
@@ -149,8 +71,6 @@ public class SAP {
 				   sol[0] = current;
 				   sol[1] = map.get(current) + mapv.get(current);
 			   }
-//			   else //if vertex is already visited with a better value do not add neighbours
-//				   continue; // early stopping
 		   }
 			   
 		   for(int adj : DG.adj(current)) {
@@ -160,13 +80,14 @@ public class SAP {
 			   }
 		   }
 	   }
-	   return sol;
+	   result.add(map);
+	   result.add(sol);
+	   return result;
    }
-   
    
    // do unit testing of this class
    public static void main(String[] args) {
-	    In in = new In("testing/digraph3.txt"); //args[0]);
+	    In in = new In("testing/digraph3.txt"); 
 	    Digraph G = new Digraph(in);
 	    SAP sap = new SAP(G);
 	    
